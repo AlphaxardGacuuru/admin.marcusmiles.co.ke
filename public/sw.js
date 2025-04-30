eb41902a3e7196a5d34ad2a2b2896ae31dcee850
Alphaxard Gacuuru <alphaxardgacuuru47@gmail.com>
2025-04-30 13:55:20 +0300

    refactor(): renamed cache

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // cacheName needs to be changed after any of the assets are changed
-const cacheName = 'MarcusMilesCache5';
+const cacheName = 'MarcusMilesCache6';
 
 const cacheAssets = [
 	'/js/app.js'
